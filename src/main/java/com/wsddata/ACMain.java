252487e8d6a1b6fb1777e551a8082f1d83b1c06e
zhaojc <zhaojc@calis.edu.cn>
2017-04-21 14:50:01 +0800

    基本结构

--- src/main/java/com/wsddata/ACMain.java
+++ src/main/java/com/wsddata/ACMain.java
@@ -7,29 +7,89 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import redis.clients.jedis.Jedis;
-import com.wsddata.Conf;
+import redis.clients.jedis.*;
+
 
 @Controller
 @EnableAutoConfiguration
 @ComponentScan
 public class ACMain {
-	private final Conf config;
-	
 	private Jedis jedis;
-	
+	private JedisPool jedisPool;
+
 	@Autowired
 	public ACMain(Conf conf){
-		this.config=conf;
-		jedis=new Jedis(config.redis_host,config.redis_port);
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMaxTotal(conf.redisMaxTotal);
+		config.setMaxIdle(conf.redisMaxIdle);
+		config.setMaxWaitMillis(conf.redisMaxWaitmillis);
+		try{
+			jedisPool=new JedisPool(config,conf.redisHost,conf.redisPort);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	
-	@RequestMapping("/")
+	@RequestMapping("/login")
     @ResponseBody
-    String home() {
-		jedis.set("user", "{'id':'1','username:lisi','organization:calis'}");
+    String login() {
+		jedis=jedisPool.getResource();
+		jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
 		String value = jedis.get("user");
-		
+        return value;
+    }
+	
+	@RequestMapping("/logout")
+    @ResponseBody
+    String logout() {
+		jedis=jedisPool.getResource();
+		jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
+		String value = jedis.get("user");
+        return value;
+    }
+	
+	@RequestMapping("/registerApp")
+    @ResponseBody
+    String registerApp() {
+		jedis=jedisPool.getResource();
+		jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
+		String value = jedis.get("user");
+        return value;
+    }
+	
+	@RequestMapping("/registerUser")
+    @ResponseBody
+    String registerUser() {
+		jedis=jedisPool.getResource();
+		jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
+		String value = jedis.get("user");
+        return value;
+    }
+	
+	@RequestMapping("/getAppInfo")
+    @ResponseBody
+    String getAppInfo() {
+		jedis=jedisPool.getResource();
+		jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
+		String value = jedis.get("user");
+        return value;
+    }
+	
+	@RequestMapping("/getUserInfo")
+    @ResponseBody
+    String getUserInfo() {
+		jedis=jedisPool.getResource();
+		jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
+		String value = jedis.get("user");
+        return value;
+    }
+	
+	@RequestMapping("/checkToken")
+    @ResponseBody
+    String checkToken() {
+		jedis=jedisPool.getResource();
+		jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
+		String value = jedis.get("user");
         return value;
     }
 	
--- src/main/java/com/wsddata/Conf.java
+++ src/main/java/com/wsddata/Conf.java
@@ -6,9 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class Conf {
 	@Value("${redis.host}")
-	public String redis_host;
+	public String redisHost;
 	
 	@Value("${redis.port}")
-	public int redis_port;
+	public int redisPort;
 	
+	@Value("${redis.max_total}")
+	public int redisMaxTotal;
+	
+	@Value("${redis.max_idle}")
+	public int redisMaxIdle;
+	
+	@Value("${redis.max_waitmillis}")
+	public int redisMaxWaitmillis;
 }
