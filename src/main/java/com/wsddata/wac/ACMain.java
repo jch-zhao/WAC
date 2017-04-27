889338e0ab3f5849111dd5d2d3a7b4cd47870088
zhaojc <zhaojc@calis.edu.cn>
2017-04-27 15:21:45 +0800

    close jedis

--- src/main/java/com/wsddata/wac/ACMain.java
+++ src/main/java/com/wsddata/wac/ACMain.java
@@ -45,29 +45,32 @@ public class ACMain {
     @ResponseBody
     String login(HttpServletRequest request) {
 		String result=null;
-		//String systemId = request.getHeader("SystemId");
-		String systemId = "test";
+		String systemId = request.getHeader("systemId");
+		System.out.println(systemId);
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		if(systemId==null||systemId.equals("")||username==null||username.equals("")||password==null){
 			result="{'successful':false,'error':'Missing parameters'}";
 			return result;
 		}
-		
-		jedis=jedisPool.getResource();
-		List<String> user=jedis.hmget(systemId+":"+username,"password");
-		
-		if(password.equals(user.get(0))){
-			String token=generateToken(systemId);
-			if(token!=null){
-				jedis.zadd("TokenPool",new Date().getTime(),token);
-				//request.getSession().setAttribute("token",token);//此处待改，应由应用系统操作
-				result="{'successful':true,'token':'"+token+"'}";
+		try{
+			jedis=jedisPool.getResource();
+			List<String> user=jedis.hmget(systemId+":"+username,"password");
+			if(password.equals(user.get(0))){
+				String token=generateToken(systemId);
+				if(token!=null){
+					jedis.zadd("TokenPool",new Date().getTime(),token);
+					result="{'successful':true,'token':'"+token+"'}";
+				}else{
+					result="{'successful':false,'error':'token fail'}";
+				}
 			}else{
-				result="{'successful':false,'error':'token fail'}";
+				result="{'successful':false,'error':'login fail'}";
 			}
-		}else{
-			result="{'successful':false,'error':'login fail'}";
+		}catch(Exception e){
+			result="{'successful':false,'error':'server error'}";
+		}finally{
+			jedis.close();
 		}
         return result;
     }
@@ -75,65 +78,113 @@ public class ACMain {
 	@RequestMapping("/admin/logout")
     @ResponseBody
     String logout(HttpServletRequest request) {
-		//jedis=jedisPool.getResource();
-		//jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
-		String value = "logout";
-        return value;
+		String result=null;
+		String username = request.getParameter("username");
+		try{
+			jedis=jedisPool.getResource();
+			//从tokenPool中清除此用户的token
+			result="";
+		}catch(Exception e){
+			result="{'successful':false,'error':'server error'}";
+		}finally{
+			jedis.close();
+		}
+        return result;
     }
 	
 	@RequestMapping("/service/registerApp")
     @ResponseBody
     String registerApp(HttpServletRequest request) {
-		//jedis=jedisPool.getResource();
-		//jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
-		String value = "registerApp";
-        return value;
+		String result=null;
+		
+		try{
+			jedis=jedisPool.getResource();
+			result="";
+		}catch(Exception e){
+			result="{'successful':false,'error':'server error'}";
+		}finally{
+			jedis.close();
+		}
+        return result;
     }
 	
 	@RequestMapping("/approval/registerUser")
     @ResponseBody
     String registerUser(HttpServletRequest request) {
-		//注册用户，必须的参数是系统Id、userId
-		//jedis=jedisPool.getResource();
-		//jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
-		String value = "approval";
-        return value;
+		String result=null;
+		
+		try{
+			jedis=jedisPool.getResource();
+			result="";
+		}catch(Exception e){
+			result="{'successful':false,'error':'server error'}";
+		}finally{
+			jedis.close();
+		}
+        return result;
     }
 	
 	@RequestMapping("/1.0/getAppInfo")
     @ResponseBody
     String getAppInfo(HttpServletRequest request) {
-		jedis=jedisPool.getResource();
-		//jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
-		String value = jedis.get("user");
-        return value;
+		String result=null;
+		
+		try{
+			jedis=jedisPool.getResource();
+			result="";
+		}catch(Exception e){
+			result="{'successful':false,'error':'server error'}";
+		}finally{
+			jedis.close();
+		}
+        return result;
     }
 	
 	@RequestMapping("/1.0/getUserInfo")
     @ResponseBody
     String getUserInfo(HttpServletRequest request) {
-		jedis=jedisPool.getResource();
-		jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
-		String value = jedis.get("user");
-        return value;
+		String result=null;
+		
+		try{
+			jedis=jedisPool.getResource();
+			result="";
+		}catch(Exception e){
+			result="{'successful':false,'error':'server error'}";
+		}finally{
+			jedis.close();
+		}
+        return result;
     }
 	
 	@RequestMapping("/1.0/changePassword")
     @ResponseBody
     String changePassword(HttpServletRequest request) {
-		jedis=jedisPool.getResource();
-		//
-		String value = jedis.get("user");
-        return value;
+		String result=null;
+		
+		try{
+			jedis=jedisPool.getResource();
+			result="";
+		}catch(Exception e){
+			result="{'successful':false,'error':'server error'}";
+		}finally{
+			jedis.close();
+		}
+        return result;
     }
 	
 	@RequestMapping("/0.1/checkToken")
     @ResponseBody
     String checkTokenTest(HttpServletRequest request) {
-		jedis=jedisPool.getResource();
-		jedis.set("user", "{'id':'1','username:wangfang','organization:calis'}");
-		String value = jedis.get("user");
-        return value;
+		String result=null;
+		try{
+			jedis=jedisPool.getResource();
+			result="";
+		}catch(Exception e){
+			result="{'successful':false,'error':'server error'}";
+		}finally{
+			jedis.close();
+		}
+        return result;
     }
 	
 	@RequestMapping("/1.0/checkToken")
@@ -141,27 +192,38 @@ public class ACMain {
     String checkToken(HttpServletRequest request) {
 		String result = null;
 		String token = request.getParameter("token");
-		
-		jedis=jedisPool.getResource();
-		if(jedis.zscore("TokenPool",token)!=null){
-			jedis.zadd("TokenPool",new Date().getTime(),token);
-			result="{'successful':true,'message':'ok'}";
-		}else{
-			result="{'successful':false,'error':'No login'}";
+		try{
+			jedis=jedisPool.getResource();
+			if(jedis.zscore("TokenPool",token)!=null){
+				jedis.zadd("TokenPool",new Date().getTime(),token);
+				result="{'successful':true,'message':'ok'}";
+			}else{
+				result="{'successful':false,'error':'No login'}";
+			}
+		}catch(Exception e){
+			result="{'successful':false,'error':'server error'}";
+		}finally{
+			jedis.close();
 		}
         return result;
     }
 
 	@Scheduled(fixedRate = 60000)//每一分钟清除过期的token
     public void cleanToken() throws InterruptedException {
-        //当前时间30分钟以前的token被删除
-		jedis=jedisPool.getResource();
-		Long now=new Date().getTime();
-		Set<String> expired=jedis.zrangeByScore("TokenPool",0,now-30*60*1000);
-		Iterator<String> i = expired.iterator();
-		System.out.println("准备清除"+expired.size()+"个token");
-		while(i.hasNext()){
-			jedis.zrem("TokenPool",i.next());
+		try{
+			//当前时间30分钟以前的token被删除
+			jedis=jedisPool.getResource();
+			Long now=new Date().getTime();
+			Set<String> expired=jedis.zrangeByScore("TokenPool",0,now-30*60*1000);
+			Iterator<String> i = expired.iterator();
+			System.out.println("准备清除"+expired.size()+"个token");
+			while(i.hasNext()){
+				jedis.zrem("TokenPool",i.next());
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			jedis.close();
 		}
     }
 
